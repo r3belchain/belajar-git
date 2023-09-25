d1c725ffbc08fb476f73585b8f3c1a220c38a7f7
azharaufa <azharaufa221105@gmail.com>
2023-09-25 09:47:27 +0100

    merevisi fungsi perkalian

--- funsi.js
+++ funsi.js
@@ -4,8 +4,8 @@ console.log(add(1, 2));
 const subtract = (a,b) => a - b;
 console.log(subtract(5, 2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 2));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 2));
 
 const halo = (a, b) => a / b;
 console.log(halo(10, 2));
